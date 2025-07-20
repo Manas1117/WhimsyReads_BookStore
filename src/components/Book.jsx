973d8bedf69c51656c0d8935efd035cdbcccdcf3
Manas1117 <manasrathod2003@gmail.com>
2025-07-21 00:36:33 +0530

    Adding Data

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -11,7 +11,8 @@ function Book() {
   useEffect(() => {
     const getBook = async()=>{
       try {
-       const res = await axios.get("https://whimsy-reads-book-store.vercel.app/book");
+      //  const res = await axios.get("https://whimsy-reads-book-store.vercel.app/book");
+      const res = await axios.get("http://localhost:4001/book");
        console.log(res.data);
        setBook(res.data);
       } catch (error) {
